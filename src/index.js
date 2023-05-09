dfd867b6cddfa21d280658a458de6d3a237af75f
Ivan Sholana <94579033+IvanSholana@users.noreply.github.com>
2023-05-10 06:31:49 +0700

    seven

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+require("./support");
+
 import { run } from "./app/app";
 import { AlertService } from "./app/alert.service";
 import { CalculatorService } from "./app/calculator.service";
 import { JokesService } from "./app/jokes.service";
-import css from "./style/style.scss";
-import * as bootstrap from "bootstrap";
 
 const alertService = new AlertService();
 const calculatorService = new CalculatorService();
 const jokesService = new JokesService();
 
-alert("Hallo Ivan hehe❤️");
+alert("Hallo Ivan he❤️");
 run(alertService, calculatorService, jokesService);
