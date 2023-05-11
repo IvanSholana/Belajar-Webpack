a9f168d901bdb32fe23f23749b96e6b5c3f59456
Ivan Sholana <94579033+IvanSholana@users.noreply.github.com>
2023-05-12 05:58:42 +0700

    ten

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,41 @@
 const path = require("path");
+const glob = require("glob");
 const { merge } = require("webpack-merge");
 const config = require("./webpack.config");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
 
 module.exports = merge(config, {
   mode: "production",
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
+    }),
+    new MiniCssExtractPlugin(),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
   ],
   output: {
